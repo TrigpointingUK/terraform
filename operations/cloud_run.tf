daa88c51fd2f62d66bd165e31a89ecec88632f41
Ian Harris <ian@teasel.org>
2022-01-31 21:46:41 +0000

    vue

--- operations/cloud_run.tf
+++ operations/cloud_run.tf
@@ -2,8 +2,8 @@ locals {
   cloudrun_services = [
     "api-tme",
     "api-tuk",
-    # "vue-tme",
-    # "vue-tuk",
+    "vue-tme",
+    "vue-tuk",
   ]
 }
 
@@ -94,4 +94,67 @@ resource "google_cloud_run_domain_mapping" "api-tuk" {
 
 # output "api_trigpointing_uk_cname" {
 #     value = google_cloud_run_domain_mapping.api-tuk.status[0].resource_records[0].rrdata
+# }
+
+
+
+# Map trigpointing.me domain
+data "google_cloud_run_service" "vue-tme" {
+  name = "vue-tme"
+  location = var.region
+}
+
+resource "google_cloud_run_domain_mapping" "vue-tme" {
+  location = var.region
+  name     = "trigpointing.me"
+  metadata {
+    namespace = var.project
+  }
+  spec {
+    route_name = data.google_cloud_run_service.vue-tme.name
+  }
+}
+
+resource "google_dns_record_set" "vue-tme" {
+  name = "${google_dns_managed_zone.tme-zone.dns_name}"
+  type = "A"
+  ttl  = 300
+  managed_zone = google_dns_managed_zone.tme-zone.name
+  rrdatas = [ for ip in google_cloud_run_domain_mapping.vue-tme.status[0].resource_records[*].rrdata : ip if length(regexall(":", ip)) == 0 ]
+}
+
+output "vue_trigpointing_me_rrdata" {
+    value = google_dns_record_set.vue-tme.rrdatas
+}
+
+
+
+# Map vue.trigpointing.uk domain
+data "google_cloud_run_service" "vue-tuk" {
+  name = "vue-tuk"
+  location = var.region
+}
+
+resource "google_cloud_run_domain_mapping" "vue-tuk" {
+  location = var.region
+  name     = "vue.trigpointing.uk"
+  metadata {
+    namespace = var.project
+  }
+  spec {
+    route_name = data.google_cloud_run_service.vue-tuk.name
+  }
+}
+
+### NB DNS nameservers still at 123-reg, so this needs to be applied manually there
+# resource "google_dns_record_set" "vue-tuk" {
+#   name = "vue.${google_dns_managed_zone.tuk-zone.dns_name}"
+#   type = "CNAME"
+#   ttl  = 300
+#   managed_zone = google_dns_managed_zone.tuk-zone.name
+#   rrdatas = [google_cloud_run_domain_mapping.vue-tuk.status[0].resource_records[0].rrdata]
+# }
+
+# output "vue_trigpointing_uk_cname" {
+#     value = google_cloud_run_domain_mapping.vue-tuk.status[0].resource_records[0].rrdata
 # }
